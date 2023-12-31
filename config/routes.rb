ee5cd49491e8a76ca666dda00df761fb7f8bb3c1
cubik228 <mkomp06@gmail.com>
2023-12-31 15:45:38 +0300

    add trash and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :uploaded_files, only: [:index, :new, :create, :destroy, :show] do
     get 'history', on: :collection
     get 'storage', on: :collection
+    collection do
+      get 'trash', to: 'uploaded_files#trash'
+      delete 'empty_trash', to: 'uploaded_files#empty_trash'
+    end
   end
 
   root 'homes#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_27_210931) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_31_120202) do
+  create_table "file_trashes", force: :cascade do |t|
+    t.string "name"
+    t.boolean "deleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "upladed_files", force: :cascade do |t|
     t.string "name"
     t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "deleted", default: false
   end
 
   create_table "uploaded_files", force: :cascade do |t|
@@ -23,6 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_210931) do
     t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "trashed"
+    t.integer "status"
   end
 
 end
